2e7eefc11c15bd5fe07275163f2aa6eee4ef2302
sai2dreams <sai2dreams@gmail.com>
2016-05-28 23:52:37 +0530

    created DAL

---
+++ CShorpApp1/CShorpApp1/DAL/dbConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CShorpApp1.DAL
+{
+    class dbConnection
+    {
+        SqlConnection cn = new SqlConnection("server=.;database=CollectionDB;Integrated Security=True;");
+        
+
+    }
+}
